110e072ff2f07e55a4929d99c312370db2a84f9b
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-19 12:11:01 -0300

    Salva usuário no db

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,6 +1,9 @@
+import { eq } from 'drizzle-orm';
 import z from 'zod';
+import { db } from '../db';
+import { usersTable } from '../db/schema';
 import { HttpRequest, HttpResponse } from '../types/Http';
-import { badRequest, created } from '../utils/http';
+import { badRequest, conflict, created } from '../utils/http';
 
 const schema = z.object({
   goal: z.enum(['lose', 'maintain', 'gain']),
@@ -24,8 +27,35 @@ export class SignUpController {
       return badRequest({ errors: error.issues });
     }
 
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true,
+      },
+      where: eq(usersTable.email, data.account.email),
+    });
+
+    if (userAlreadyExists) {
+      return conflict({ error: 'This email is already in use.' });
+    }
+
+    const { account, ...rest } = data;
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({
+        ...account,
+        ...rest,
+        calories: 0,
+        carbohydrates: 0,
+        fats: 0,
+        proteins: 0,
+      })
+      .returning({
+        id: usersTable.id,
+      });
+
     return created({
-      data,
+      userId: user.id,
     });
   }
 }
